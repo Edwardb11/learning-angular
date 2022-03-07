1a06121825c13dac405e64bb98a5106e93ccd03c
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-07 15:20:06 -0400

    instalando bootstrap

--- basic/src/app/personas/personas.component.ts
+++ basic/src/app/personas/personas.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css'],
 })
 export class PersonasComponent {}
