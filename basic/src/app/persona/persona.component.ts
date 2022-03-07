d29e1d6ef5d82cb066e0a1119d9880b99e891d1a
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-07 15:56:18 -0400

    interpolacion de variables

--- basic/src/app/persona/persona.component.ts
+++ basic/src/app/persona/persona.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { NumberSymbol } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-persona',
   templateUrl: './persona.component.html',
-  styleUrls: ['./persona.component.css']
+  styleUrls: ['./persona.component.css'],
 })
 export class PersonaComponent {
+  nombre: string = 'Edward';
+  apellido: string = 'Brito';
+  edad: number = 20;
 
-  constructor() { }
-
+  // atributos privados
+  // private edad: number = 20;
+  // getEdad(): number {
+  //   return this.edad;
+  // }
 }
