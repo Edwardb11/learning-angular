f8246ec26a73c3d874d018f10a2f5b4bee409fdc
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-08 11:01:57 -0400

    update design

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -3,15 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   titulo = 'Aplicación Calculadora';
   operandoA: number = 0;
   operandoB: number = 0;
   resultado: number = 0;
-
-  sumar():void{
+  sumar(): void {
     this.resultado = this.operandoA + this.operandoB;
   }
 }
