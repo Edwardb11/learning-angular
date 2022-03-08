ef422c723cbc53185d66ba711bf70dd4c9b2d963
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-08 14:28:10 -0400

    comunicacion entre componente hijo e padre

--- listado-personas/src/app/app.component.ts
+++ listado-personas/src/app/app.component.ts
@@ -4,20 +4,16 @@ import { Persona } from './persona.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   titulo = 'Listado de Personas';
   personas: Persona[] = [
-    new Persona('Juan','Perez'), 
+    new Persona('Juan', 'Perez'),
     new Persona('Laura', 'Juarez'),
-    new Persona('Karla', 'Lara')
+    new Persona('Karla', 'Lara'),
   ];
-  nombreInput:string = '';
-  apellidoInput:string = '';
-
-  agregarPersona(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personas.push( persona1 );
+  personaAgregada(persona: Persona) {
+    this.personas.push(persona);
   }
 }
---
+++ listado-personas/src/app/formulario/formulario.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Persona } from './../persona.model';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css'],
+})
+export class FormularioComponent {
+  // Salida de datos de componente hijo a componente padre
+  @Output() personaCreada = new EventEmitter();
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  agregarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    // this.personas.push(persona1);
+    // ahora podemos propagar informacion al componente padre
+    this.personaCreada.emit(persona1);
+  }
+}
