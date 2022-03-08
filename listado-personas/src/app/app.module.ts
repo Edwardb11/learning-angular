0ddb59985321bc5aa829c70326910349ff2e4d15
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-08 14:19:51 -0400

    generando componente de clase persona y utilizando directivas @input

--- listado-personas/src/app/app.module.ts
+++ listado-personas/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PersonaComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
