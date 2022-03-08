ecee6a79777922754fefa7daf4fb7a06f0997a70
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-08 13:25:05 -0400

    clase persona

---
+++ listado-personas/src/app/persona.model.ts
@@ -0,0 +1,13 @@
+export class Persona {
+  /*
+  nombre: string = '';
+  apellido: string = '';
+
+  constructor(nombre: string, apellido: string) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }
+  */
+  //  sintaxis simplificada
+  constructor(public nombre: string, public apellido: string) {}
+}
