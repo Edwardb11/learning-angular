01bac1f4ca4a58d047e6b91f57249ca7e55dfa2d
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-09 10:18:26 -0400

    referencia en angular

--- listado-personas/src/app/formulario/formulario.component.ts
+++ listado-personas/src/app/formulario/formulario.component.ts
@@ -9,11 +9,14 @@ import { Persona } from './../persona.model';
 export class FormularioComponent {
   // Salida de datos de componente hijo a componente padre
   @Output() personaCreada = new EventEmitter();
-  nombreInput: string = '';
-  apellidoInput: string = '';
+  // nombreInput: string = '';
+  // apellidoInput: string = '';
 
-  agregarPersona() {
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+  agregarPersona(
+    NombreInput: HTMLInputElement,
+    ApellidoInput: HTMLInputElement
+  ) {
+    let persona1 = new Persona(NombreInput.value, ApellidoInput.value);
     // this.personas.push(persona1);
     // ahora podemos propagar informacion al componente padre
     this.personaCreada.emit(persona1);
