2f6498cae5a02957905bf6181657e6da5cc9fa7f
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-11 14:06:15 -0400

    trabajando con los egresos

--- presupuesto-app/src/app/egreso/egreso.component.ts
+++ presupuesto-app/src/app/egreso/egreso.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Egreso } from './egreso.model';
+import { EgresoServicio } from './egreso.service';
 
 @Component({
   selector: 'app-egreso',
   templateUrl: './egreso.component.html',
-  styleUrls: ['./egreso.component.css']
+  styleUrls: ['./egreso.component.css'],
 })
 export class EgresoComponent implements OnInit {
-
-  constructor() { }
+  egresos: Egreso[] = [];
+  @Input() ingresoTotal: number;
+  constructor(private EgresoServicio: EgresoServicio) {}
 
   ngOnInit(): void {
+    this.egresos = this.EgresoServicio.egresos;
   }
-
+  EliminarEgresos(egresos: Egreso) {
+    this.EgresoServicio.Eliminar(egresos);
+  }
+  calcularPorcentaje(egresos: Egreso) {
+    return egresos.valor / this.ingresoTotal;
+  }
 }
--- presupuesto-app/src/app/egreso/egreso.service.ts
+++ presupuesto-app/src/app/egreso/egreso.service.ts
@@ -4,4 +4,10 @@ export class EgresoServicio {
     new Egreso('Renta departamento', 900),
     new Egreso('Ropa', 500),
   ];
+  Eliminar(egreso: Egreso) {
+    // retornar el indice del ingreso que nos mandan
+    const indice: number = this.egresos.indexOf(egreso);
+    // borrar el indice del elemento que queremos eliminar
+    this.egresos.splice(indice, 1);
+  }
 }
