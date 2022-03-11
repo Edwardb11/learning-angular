d903ff3c05393a45500a39d1035a283ba89582f2
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-11 11:49:18 -0400

    manejando el listado de los ingresos

--- presupuesto-app/src/app/ingreso/ingreso.component.ts
+++ presupuesto-app/src/app/ingreso/ingreso.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { IngresoServicio } from './ingreso.service';
+import { Ingreso } from './ingreso.model';
 
 @Component({
   selector: 'app-ingreso',
   templateUrl: './ingreso.component.html',
-  styleUrls: ['./ingreso.component.css']
+  styleUrls: ['./ingreso.component.css'],
 })
 export class IngresoComponent implements OnInit {
+  // Inicializar ingresos
+  ingresos: Ingreso[] = [];
+  // inicializar el servicio de los ingresos
+  constructor(private IngresoServicio: IngresoServicio) {}
 
-  constructor() { }
-
+  // asignar a los servicio los ingresos
   ngOnInit(): void {
+    this.ingresos = this.IngresoServicio.ingresos;
   }
-
+  EliminarRegistro(ingreso: Ingreso) {
+    // aqui usamos el metodo eliminar que creamos en nuestro servicio
+    this.IngresoServicio.Eliminar(ingreso);
+  }
 }
--- presupuesto-app/src/app/ingreso/ingreso.service.ts
+++ presupuesto-app/src/app/ingreso/ingreso.service.ts
@@ -4,4 +4,10 @@ export class IngresoServicio {
     new Ingreso('Salario', 4000),
     new Ingreso('Venta coche', 500),
   ];
+  Eliminar(ingreso: Ingreso) {
+    // retornar el indice del ingreso que nos mandan
+    const indice: number = this.ingresos.indexOf(ingreso);
+    // borrar el indice del elemento que queremos eliminar
+    this.ingresos.splice(indice, 1);
+  }
 }
