ceaa3167aee465276a8a65bced0fcfdb895ebf19
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-11 10:15:06 -0400

    creando automaticamente los componentes necesario para dividir todo

--- presupuesto-app/src/app/app.module.ts
+++ presupuesto-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresoComponent,
+    CabeceroComponent,
+    EgresoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
