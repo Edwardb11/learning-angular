ec771ac76d5096f69dc80b9be5b07a211815876f
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-14 11:27:24 -0400

    child routes en angular

--- listado-personas/src/app/app-routing.module.ts
+++ listado-personas/src/app/app-routing.module.ts
@@ -4,16 +4,19 @@ import { FormularioComponent } from './personas/formulario/formulario.component'
 import { PersonasComponent } from './personas/personas.component';
 
 const routes: Routes = [
-  {path: '', component: PersonasComponent},
-  {path: 'personas', component: PersonasComponent},
-  {path: 'personas/agregar', component: FormularioComponent},
-  {path: 'personas/:id', component: FormularioComponent}
-]
+  { path: '', component: PersonasComponent },
+  {
+    path: 'personas',
+    component: PersonasComponent,
+    children: [
+      { path: 'agregar', component: FormularioComponent },
+      { path: ':id', component: FormularioComponent },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(
-    routes
-  )],
-  exports:[RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
