9bd1c17c022174328d906459b7fa9fd1e24a8d82
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-14 10:50:36 -0400

    modificar personas

--- listado-personas/src/app/persona.service.ts
+++ listado-personas/src/app/persona.service.ts
@@ -17,4 +17,15 @@ export class PersonasService {
     this.loggingService.enviaMensajeAConsola('agregamos persona:' + persona.nombre)
     this.personas.push(persona);
   }
+
+  encontrarPersona(index: number){
+    let persona: Persona = this.personas[index];
+    return persona;
+  }
+
+  modificarPersona(index:number, persona: Persona){
+    let persona1 = this.personas[index];
+    persona1.nombre = persona.nombre;
+    persona1.apellido = persona.apellido;
+  }
 }
--- listado-personas/src/app/personas/formulario/formulario.component.ts
+++ listado-personas/src/app/personas/formulario/formulario.component.ts
@@ -1,5 +1,12 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoggingService } from '../../LoggingService.service';
 import { Persona } from '../../persona.model';
 import { PersonasService } from '../../persona.service';
@@ -7,19 +14,35 @@ import { PersonasService } from '../../persona.service';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
-export class FormularioComponent  {
-  nombreInput:string;
-  apellidoInput:string;  
+export class FormularioComponent implements OnInit {
+  nombreInput: string;
+  apellidoInput: string;
+  index: number;
 
-  constructor(private personaService:PersonasService,
-              private router: Router){}
+  constructor(
+    private personaService: PersonasService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  guardarPersona(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personaService.agregarPersona(persona1);
-    this.router.navigate(['personas']);
+  ngOnInit(): void {
+    this.index = this.route.snapshot.params['id'];
+    if (this.index) {
+      let persona: Persona = this.personaService.encontrarPersona(this.index);
+      this.nombreInput = persona.nombre;
+      this.apellidoInput = persona.apellido;
+    }
   }
 
+  guardarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    if (this.index) {
+      this.personaService.modificarPersona(this.index, persona1);
+    } else {
+      this.personaService.agregarPersona(persona1);
+    }
+    this.router.navigate(['personas']);
+  }
 }
