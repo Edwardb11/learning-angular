acaa3f0b7eedec808408c6f30ce46d1dc7050c35
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-15 12:35:13 -0400

    trabajando con la paginacion funcionando los botones

--- pokeApp-angular-material/src/app/poke/grid/grid.component.ts
+++ pokeApp-angular-material/src/app/poke/grid/grid.component.ts
@@ -9,13 +9,29 @@ import { PokeService } from './../../services/poke.service';
 export class GridComponent {
   // para almacenar lo que nos retorna el observable del servicio de la peticion get
   pokeList: Array<any> = [];
+
+  // iniciar la pagina en 0
+  page: number = 0;
+
   // utilizando el servicio
   constructor(private PokeService: PokeService) {
     // llamamos al metodo que creamos
     //  este peticion nos retorna un observable rjx y nos subscribimos y nos permite pasarle una funcion flecha para la respuesta
     PokeService.getList().subscribe((res: any) => {
       this.pokeList = res.results;
-      console.log(res);
+      // console.log(res);
+    });
+  }
+  nextPage() {
+    this.page += 8;
+    this.PokeService.getList(this.page).subscribe((res: any) => {
+      this.pokeList = res.results;
+    });
+  }
+  prevPage() {
+    this.page -= 8;
+    this.PokeService.getList(this.page).subscribe((res: any) => {
+      this.pokeList = res.results;
     });
   }
 }
