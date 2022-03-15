cbfcf84bca9ac167357f988cacb0183d2bf4059d
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-15 11:09:22 -0400

    card y grid

--- pokeApp-angular-material/src/app/poke/poke.module.ts
+++ pokeApp-angular-material/src/app/poke/poke.module.ts
@@ -4,9 +4,12 @@ import { GridComponent } from './grid/grid.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardComponent } from './card/card.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [GridComponent, NavbarComponent, CardComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatGridListModule, MatCardModule],
   exports: [GridComponent],
 })
 export class PokeModule {}
