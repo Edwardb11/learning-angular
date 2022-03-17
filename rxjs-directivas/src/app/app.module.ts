d0d78f197614319d061adc663129c9059ab81560
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-17 10:27:43 -0400

    base para trabajar

--- rxjs-directivas/src/app/app.module.ts
+++ rxjs-directivas/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveModule } from './reactive/reactive.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
