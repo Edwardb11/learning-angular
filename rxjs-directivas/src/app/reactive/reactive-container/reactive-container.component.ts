e794e15000b7d686714495eea3fbd7211e35dd94
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-17 10:30:12 -0400

    creando observable

--- rxjs-directivas/src/app/reactive/reactive-container/reactive-container.component.ts
+++ rxjs-directivas/src/app/reactive/reactive-container/reactive-container.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-container',
   templateUrl: './reactive-container.component.html',
-  styleUrls: ['./reactive-container.component.css']
+  styleUrls: ['./reactive-container.component.css'],
 })
 export class ReactiveContainerComponent implements OnInit {
+  numero = 0;
+  miObservable = new Observable<number>(() => {
+    setInterval(() => {
+      this.numero++;
+    }, 3000);
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
