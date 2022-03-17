fa6b169c36ef42a78d2175187d91735926389570
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-17 12:32:30 -0400

    intervalo dentro de rxjs

--- rxjs-directivas/src/app/reactive/reactive-container/reactive-container.component.ts
+++ rxjs-directivas/src/app/reactive/reactive-container/reactive-container.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { observable, Observable, observeOn } from 'rxjs';
+import { Observable, interval, observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-container',
@@ -7,18 +7,22 @@ import { observable, Observable, observeOn } from 'rxjs';
   styleUrls: ['./reactive-container.component.css'],
 })
 export class ReactiveContainerComponent implements OnInit {
+  // Interval integtrado de RXJS
+
+  miIntervalo: Observable<number> = interval(3000);
+
   constructor() {
-    let numero = 0;
-    const miObservable = new Observable<number>((observer) => {
-      setInterval(() => {
-        numero++;
-        observer.next(numero);
-        observer.complete();
-        if (numero == 3) {
-          observer.error('Numero error');
-        }
-      }, 3000);
-    });
+    // let numero = 0;
+    // const miObservable = new Observable<number>((observer) => {
+    //   setInterval(() => {
+    //     numero++;
+    //     observer.next(numero);
+    //     observer.complete();
+    //     if (numero == 3) {
+    //       observer.error('Numero error');
+    //     }
+    //   }, 3000);
+    // });
     // Legacy
     // llamar al observable
     // miObservable.subscribe(
@@ -33,20 +37,21 @@ export class ReactiveContainerComponent implements OnInit {
     //     console.log('Observable completado');
     //   }
     // );
-
     // Forma moderna
-    miObservable.subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (error) => {
-        console.log(error);
-      },
-      complete: () => {
-        console.log('Observable completado');
-      },
-    });
+    // miObservable.subscribe({
+    //   next: (res) => {
+    //     console.log(res);
+    //   },
+    //   error: (error) => {
+    //     console.log(error);
+    //   },
+    //   complete: () => {
+    //     console.log('Observable completado');
+    //   },
+    // });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.miIntervalo.subscribe((value) => console.log(value));
+  }
 }
