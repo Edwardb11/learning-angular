11f07b4f0c36537afb340bb3ea38a74488faff1f
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-17 15:13:41 -0400

    nodularizando componentes

--- tareas/src/app/tareas/tareas.module.ts
+++ tareas/src/app/tareas/tareas.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubtituloComponent } from './subtitulo/subtitulo.component';
 import { ListadoTareasComponent } from './listado-tareas/listado-tareas.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
-  declarations: [SubtituloComponent, ListadoTareasComponent],
+  declarations: [SubtituloComponent, ListadoTareasComponent, GridComponent],
   imports: [CommonModule],
-  exports: [SubtituloComponent, ListadoTareasComponent],
+  exports: [GridComponent],
 })
 export class TareasModule {}
