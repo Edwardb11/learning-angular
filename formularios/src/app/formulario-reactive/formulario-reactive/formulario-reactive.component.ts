fc166f26acbd15418329973559a3f9b4564c67f0
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-18 15:00:22 -0400

    formn control para formularios reactivps

--- formularios/src/app/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
+++ formularios/src/app/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactive',
   templateUrl: './formulario-reactive.component.html',
-  styleUrls: ['./formulario-reactive.component.css']
+  styleUrls: ['./formulario-reactive.component.css'],
 })
 export class FormularioReactiveComponent implements OnInit {
+  MiFormulario: FormGroup = new FormGroup({
+    proyecto: new FormControl(''),
+    horas: new FormControl(0),
+    tecnologias: new FormControl(''),
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
