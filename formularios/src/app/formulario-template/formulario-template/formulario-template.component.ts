a9fa18823429b3b953486dccadbb09f58b2228fc
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-18 12:12:14 -0400

    capturando del input

--- formularios/src/app/formulario-template/formulario-template/formulario-template.component.ts
+++ formularios/src/app/formulario-template/formulario-template/formulario-template.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-formulario-template',
   templateUrl: './formulario-template.component.html',
-  styleUrls: ['./formulario-template.component.css']
+  styleUrls: ['./formulario-template.component.css'],
 })
 export class FormularioTemplateComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  agregar() {
+    console.log('enviado');
   }
-
 }
