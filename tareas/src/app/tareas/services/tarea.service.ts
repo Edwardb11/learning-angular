4c89afe09df881488649bf4dd334717b15a049bf
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-18 09:48:01 -0400

    implementando una interface

--- tareas/src/app/tareas/services/tarea.service.ts
+++ tareas/src/app/tareas/services/tarea.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Tarea } from '../interface/tarea.interface';
 
 @Injectable()
 export class tareasServices {
-  tareas: Array<string> = ['Barrer', 'Trapear', 'Cocinar', 'Leer'];
+  tareas: Array<Tarea> = [
+    { tarea: 'Barrer', completada: false },
+    { tarea: 'Trapear', completada: false },
+    { tarea: 'Cocinar', completada: true },
+    { tarea: 'Leer', completada: false },
+  ];
+  deleteTarea() {
+    this.tareas.pop();
+  }
 }
