7b371572b364e4dd80a2c3da74e9a2d0b0ac95ac
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-25 11:41:06 -0400

    agregando estilos y datos

---
+++ portafolio/src/app/shared/data/Proyectos.data.ts
@@ -0,0 +1,18 @@
+export const proyectos: any[] = [
+  {
+    nombre: 'Pokeapp',
+    img: '/assets/pokeapp.png',
+    tecnologias: ['Angular Framework', 'Angular Material', 'PWA'],
+    descripcion:
+      'Aplicacion de pokemon usando Angular Material para los estilos',
+    url: 'https://vibrant-jennings-9590cf.netlify.app/',
+  },
+  {
+    nombre: 'Listado de Tareas',
+    img: '/assets/tareas.png',
+    tecnologias: ['Angular Framework', 'Bulma CSS'],
+    descripcion:
+      'Aplicacion de listado de tareas usando Bulma CSS como Framework para los estilos',
+    url: 'https://thirsty-elion-506cc8.netlify.app/',
+  },
+];
