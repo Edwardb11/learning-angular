3a893ed9997078dcc568e517ecacdac8b595a873
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-25 14:14:36 -0400

    mi primera ruta

--- portafolio/src/app/pages/pages-routing.module.ts
+++ portafolio/src/app/pages/pages-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./pages/pages.module').then((m) => m.PagesModule),
+    component: ProyectosComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class PagesRoutingModule {}
