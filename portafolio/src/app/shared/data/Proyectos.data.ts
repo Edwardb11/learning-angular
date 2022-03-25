c4c32ba092f6a6fd7b3391578defc5861068738d
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-25 13:30:41 -0400

    shared module

--- portafolio/src/app/shared/data/Proyectos.data.ts
+++ portafolio/src/app/shared/data/Proyectos.data.ts
@@ -1,18 +1,24 @@
 export const proyectos: any[] = [
   {
     nombre: 'Pokeapp',
-    img: '/assets/pokeapp.png',
-    tecnologias: ['Angular Framework', 'Angular Material', 'PWA'],
-    descripcion:
-      'Aplicacion de pokemon usando Angular Material para los estilos',
-    url: 'https://vibrant-jennings-9590cf.netlify.app/',
+    img: '',
+    tecnologias: ['Angular Framework'],
+    descripcion: '',
+    url: 'https://forms-q20j25a75-edwardb11.vercel.app/',
+  },
+  {
+    nombre: 'CRUD',
+    img: '',
+    tecnologias: ['Angular Framework'],
+    descripcion: '',
+    url: 'https://crud-6xqyued3h-edwardb11.vercel.app/',
   },
   {
     nombre: 'Listado de Tareas',
-    img: '/assets/tareas.png',
+    img: '',
     tecnologias: ['Angular Framework', 'Bulma CSS'],
     descripcion:
       'Aplicacion de listado de tareas usando Bulma CSS como Framework para los estilos',
-    url: 'https://thirsty-elion-506cc8.netlify.app/',
+    url: 'https://tasks-qg3qhjyrs-edwardb11.vercel.app/',
   },
 ];
